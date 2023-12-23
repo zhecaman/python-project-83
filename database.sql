11377d0addd80090d03add126c2bb29f1bb80379
zhecaman <zhecaman@icloud.com>
2023-12-23 17:48:03 +0300

    edited sql script

--- database.sql
+++ database.sql
@@ -1,21 +1,20 @@
+DROP TABLE IF EXISTS url_checks CASCADE;
 DROP TABLE IF EXISTS urls CASCADE;
-
-
 CREATE TABLE urls (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name varchar(255) NOT NULL,
-    created_at DATE
-    );
-
-
-DROP TABLE IF EXISTS url_checks;
+    id SERIAL,
+    name VARCHAR(255) UNIQUE,
+    created_at DATE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE url_checks (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id bigint REFERENCES urls (id),
-    status_code integer,
-    h1 text,
-    title text,
-    description text,
-    created_at DATE NOT NULL
-  );
+    id SERIAL, 
+    url_id INTEGER, 
+    status_code INTEGER, 
+    h1 VARCHAR(255), 
+    title VARCHAR(255), 
+    description VARCHAR(255),
+    created_at DATE DEFAULT CURRENT_TIMESTAMP, 
+    PRIMARY KEY (id),
+    FOREIGN KEY(url_id) REFERENCES urls (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
